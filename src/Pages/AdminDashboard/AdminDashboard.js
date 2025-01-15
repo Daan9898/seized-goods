b30cf27aaf3a343c912f4b2827b29112d24d8a66
= <lukasilverio94@gmail.com>
2025-01-15 22:55:06 +0100

    new style for sidebar

--- src/Pages/AdminDashboard/AdminDashboard.js
+++ src/Pages/AdminDashboard/AdminDashboard.js
@@ -1,19 +1,9 @@
-import AddNewItemForm from "../../Components/AddNewItemForm";
-import { Link } from "react-router-dom";
 import Sidebar from "../../Components/Sidebar";
 
 const AdminDashboard = () => {
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="bg-gray-100 flex-1">
       <Sidebar />
-      <h1 className="text-2xl font-bold">Admin Dashboard</h1>
-      <Link
-        to="/create-item"
-        className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded"
-      >
-        Create New Item
-      </Link>
-      <AddNewItemForm />
     </div>
   );
 };
